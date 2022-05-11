610c0ac182e7d7b34357a141982cb152071c445e
Louisiane777 <74965687+Louisiane777@users.noreply.github.com>
2022-05-11 09:24:12 +0100

    Add files via upload

---
+++ JS/function.js
@@ -0,0 +1,30 @@
+function bookNow () { alert("20% off on your first booking"); } // Display message
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+  // opens video in full screen
+  function openFullscreen(){
+    if (elem.requestFullscreen){
+      elem.requestFullscreen();
+    }
+    else if (elem.webkitRequestFullscreen){
+  /* Safari */
+      elem.webkitRequestFullscreen();
+    }
+    else if (elem.msRequestFullscreen){
+  /* IE11*/
+      elem.msRequestFullscreen();    
+    }
+  }
---
+++ JS/video.js
@@ -0,0 +1,5 @@
+// Open video in fullscreen mode
+var elem =  document.getElementById("myvideo");
+
+// when user click on the button, the video opens in full screen
+window.onclick = function() {openFullscreen()};
